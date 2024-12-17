eee8e202e81bc83fcd3f4b0e1893ff0123e74e7e
jikokuaice <134251470+JikokuAice@users.noreply.github.com>
2024-12-17 22:48:54 +0545

    initial

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseTrackerSystem;
+
+public partial class App : Application
+{
+   public App()
+   {
+      InitializeComponent();
+
+      MainPage = new MainPage();
+   }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTrackerSystem;
+
+public partial class MainPage : ContentPage
+{
+   public MainPage()
+   {
+      InitializeComponent();
+   }
+}
